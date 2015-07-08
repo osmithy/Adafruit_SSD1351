26f8a63b369b7720b8e4534a1847f426dd1a42f7
osmithy <owen@othermachine.co>
2015-07-08 13:55:08 -0700

    Removed Wprogram include from Adafruit GFX.h

--- firmware/Adafruit_GFX.h
+++ firmware/Adafruit_GFX.h
@@ -1,13 +1,6 @@
 #ifndef _ADAFRUIT_GFX_H
 #define _ADAFRUIT_GFX_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
- #include "Print.h"
-#else
- #include "WProgram.h"
-#endif
-
 #define swap(a, b) { int16_t t = a; a = b; b = t; }
 
 class Adafruit_GFX : public Print {
